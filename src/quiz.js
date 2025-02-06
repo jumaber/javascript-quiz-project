451769a6b2c54aaef2e6b4d1add976ee2c3ec3dc
Julia Mari Bernaus <jumaber@gmail.com>
2025-02-06 19:48:15 +0100

    Solved

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,47 @@
-class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+class Quiz { 
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions; // Stores the questions array
+        this.timeLimit = timeLimit; // Stores the total time limit
+        this.timeRemaining = timeRemaining; // Stores the remaining time
+        this.correctAnswers = 0; // Initializes the correct answer count
+        this.currentQuestionIndex = 0; // Initializes the correct answer count
+    }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]; // Initializes the correct answer count
+    }
 
-    // 4. shuffleQuestions()
+    moveToNextQuestion(){
+        this.currentQuestionIndex++; // Moves on to the next question
+    }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions(){
+        this.questions.sort(() => Math.random() - 0.5); // Shuffles randomly with the Fisher-Yates algorithm the questions
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer){
+        if (this.getQuestion().answer === answer){ // It checks if the questions is correct
+            this.correctAnswers++; // It adds it to the correct Answers array
+           }  
+    }
+
+    hasEnded(){
+        if (this.currentQuestionIndex < this.questions.length){ // Continues because it reads that it has not reached the end of the questions array.
+            return false;
+        }
+        else if (this.currentQuestionIndex === this.questions.length){ // It stops because it reads that it has reached the end of the questions array.
+            return true;
+        }
+    }
+
+    filterQuestionsByDifficulty(){
+        // constructor(difficulty){}
+        
+    }
+
+    averageDifficulty(){
+        
+    }
 }
+
+
